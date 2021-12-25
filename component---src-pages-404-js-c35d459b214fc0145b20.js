10be1f99610b1a9f719dff19c49041f37dc61f25
Toby Bridle <bridle.toby@gmail.com>
2021-12-25 00:14:14 +0000

    Updates

---
+++ component---src-pages-404-js-c35d459b214fc0145b20.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[883],{9616:function(e,t,n){n.r(t);var u=n(7294),l=n(529);t.default=function(){return u.createElement(l.default,{documentTitle:"Page Not Found"},u.createElement("h4",null,"Sorry, Page not Found!"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-c35d459b214fc0145b20.js.map
