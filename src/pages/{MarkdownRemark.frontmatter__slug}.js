7529f7ed88b83bd110c3080dcf9fe6442a3a4905
Toby Bridle <bridle.toby@gmail.com>
2021-12-27 17:15:29 +0000

    Fixed Ugly Letter Spacing

--- src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -1,13 +1,12 @@
 import * as React from "react";
 import { graphql } from "gatsby";
 
-// export default function MarkdownTemplate({ data }){
+export default function MarkdownTemplate({ data }){
 
-//     const { allMarkdownRemark: { nodes: pages} } = data;
-//     pages.forEach(page => {
-//         const { frontmatter, excerpt, timeToRead } = page;
-//         return (
-//         )
-//     })
-
-// }
+    return (
+        <>
+            <h1>Articles haven't been made yet</h1>
+            <h2>Reading Articles hasn't been implemented as of 27/12/2021 - check back another time</h2>
+        </>
+    )
+}
