1e7e4919822ff4959a176527c242ef5e8a83294c
Toby Bridle <bridle.toby@gmail.com>
2022-01-18 21:35:31 +0000

    Updates

--- component---src-pages-404-js-c287d004de9d4ff861ff.js
+++ component---src-pages-404-js-c287d004de9d4ff861ff.js
@@ -1,26 +1,2 @@
-"use strict";
-(self.webpackChunkblog = self.webpackChunkblog || []).push([
-  [883],
-  {
-    9616: function (e, t, n) {
-      n.r(t);
-      var o = n(7294),
-        l = n(6249);
-      t.default = function () {
-        return o.createElement(
-          l.default,
-          {
-            meta: {
-              title: "404 - Page not Found",
-              description:
-                "Hmm. There doesn't seem to be an article at this URL.",
-              robots: "noindex,follow",
-            },
-          },
-          o.createElement("h4", null, "Sorry, Page not Found!")
-        );
-      };
-    },
-  },
-]);
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[883],{9616:function(e,t,n){n.r(t);var o=n(7294),l=n(6249);t.default=function(){return o.createElement(l.default,{meta:{title:"404 - Page not Found",description:"Hmm. There doesn't seem to be an article at this URL.",robots:"noindex,follow"}},o.createElement("h4",null,"Sorry, Page not Found!"))}}}]);
 //# sourceMappingURL=component---src-pages-404-js-c287d004de9d4ff861ff.js.map
